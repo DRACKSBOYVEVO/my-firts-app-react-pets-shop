53c0a24b50ffce20a3d8e4fdb8bd7fbd2f1a07be
Administrador <Alkabiz@hotmail.com>
2023-09-24 21:03:11 -0500

    feat: nuevo componente de redirección 404

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
-import Home from "./pages/Home"
-import Sobre from "./pages/Sobre"
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+import PageNotFound from './pages/PageNotFound'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sobre' element={<Sobre />} />
+        <Route path='*' element = { <PageNotFound/> }/>
       </Routes>
     </Router>
   );
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PageNotFound = () => {
+  return (
+    <div className="container">
+        <h2 className="title-page">Página no encontrada</h2>
+        <p>Lo sentimos, la página que estás buscando no ha sido encontrada.</p>
+    </div>
+  );
+};
+
+export default PageNotFound;
