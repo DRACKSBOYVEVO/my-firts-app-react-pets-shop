220b115f82cd487b4aa40e444b52587dc0b1e6ea
Administrador <Alkabiz@hotmail.com>
2023-09-24 21:14:00 -0500

    refact: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,46 @@
-import image from "../assets/img/doguito.svg"
-import "../assets/css/componentes/header.css"
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Image from '../assets/img/doguito.svg'
+import '../assets/css/componentes/header.css'
 
 const Header = () => {
+    const [menuActive, setMenuActive] = useState(false);
+  
+    const toggleMenu = () => {
+      setMenuActive(!menuActive);
+    };
+  
     return (
-        <header className="header container">
-            <div className="menu-hamburguer">
-                <span className="menu-hamburguer__icon"></span>
-            </div>
-            <div className="header-container">
-                <a href="/" className="flex flex--center">
-                    <img className="header__logo" src={image} alt="doguito" />
-                    <h1 className="header__title">Petshop</h1>
-                </a>
-            </div>
-            <nav className="menu-header">
-                <ul className="menu-items">
-                    <li><a className="menu-item menu-item--entrar" href="#">Entrar</a></li>
-                    <li><a className="menu-item" href="#">Productos</a></li>
-                    <li><a className="menu-item" href="/">Blog</a></li>
-                    <li><a className="menu-item" href="/sobre">Sobre</a></li>
-                </ul>
-            </nav>
-            <div className="menu-header-background"></div>
-        </header>
-    )
-}
+      <header className={`header container ${menuActive ? 'menu-header--activo' : ''}`}>
+        <div className="menu-hamburguer" onClick={toggleMenu}>
+          <span className={`menu-hamburguer__icon ${menuActive ? 'menu-hamburguer--active' : ''}`}></span>
+        </div>
+        <div className="header-container">
+          <Link to="/" className="flex flex--center">
+            <img className="header__logo" src={Image} alt="doguito" />
+            <h1 className="header__title">Petshop</h1>
+          </Link>
+        </div>
+        <nav className="menu-header">
+          <ul className="menu-items">
+            <li>
+              <Link className="menu-item menu-item--entrar" to="#">Entrar</Link>
+            </li>
+            <li>
+              <Link className="menu-item" to="#">Productos</Link>
+            </li>
+            <li>
+              <Link className="menu-item" to="/">Blog</Link>
+            </li>
+            <li>
+              <Link className="menu-item" to="/sobre">Sobre</Link>
+            </li>
+          </ul>
+          <div className="menu-header__fechar" onClick={toggleMenu}></div>
+        </nav>
+        <div className={`menu-header-background ${menuActive ? 'menu-header-background--activo' : ''}`} onClick={toggleMenu}></div>
+      </header>
+    );
+  };
 
 export default Header
