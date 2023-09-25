0e3fda8fdbd913ff38110b8ca408539b3b57ef92
Administrador <Alkabiz@hotmail.com>
2023-09-24 21:41:51 -0500

    refact: componente para listar las categorias

--- src/pages/ListCategories.jsx
+++ src/pages/ListCategories.jsx
@@ -4,26 +4,27 @@ import { buscar } from "../api/api";
 import "../assets/css/blog.css"
 
 const ListCategories = () => {
-
+    // Definimos un estado local 'categories' para almacenar la lista de categorías.
     const [categories, setCategories] = useState([]);
-
+  
+    // Utilizamos 'useEffect' para cargar la lista de categorías cuando el componente se monta.
     useEffect(() => {
-        buscar(`/categorias`, setCategories)
-    }, [])
-
+      // Llamamos a la función 'buscar' de la API para obtener la lista de categorías.
+      buscar('/categorias', setCategories);
+    }, []);
+  
     return (
-        <ul className="category-list container flex">
-            {
-                categories.map(category => (
-                    <Link to={`/categoria/${category.id}`} key={category.id}>
-                        <li className={`category-list__category category-list__category--${category.id}`}>
-                            {category.nombre}
-                        </li>
-                    </Link>
-                ))
-            }
-        </ul>
-    )
-}
-
-export default ListCategories;
+      // Renderizamos una lista de categorías en forma de enlaces.
+      <ul className="category-list container flex">
+        {categories.map(category => (
+          <Link to={`/categoria/${category.id}`} key={category.id}>
+            <li className={`category-list__category category-list__category--${category.id}`}>
+              {category.nombre}
+            </li>
+          </Link>
+        ))}
+      </ul>
+    );
+  };
+  
+  export default ListCategories;
