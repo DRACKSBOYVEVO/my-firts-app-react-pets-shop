f7ef320a390f535e5560d56f65b635c3cb6ff542
Administrador <Alkabiz@hotmail.com>
2023-09-24 21:44:01 -0500

    docs: componente para listar subcategorias

--- src/pages/SubCategoria.jsx
+++ src/pages/SubCategoria.jsx
@@ -2,10 +2,13 @@ import { useParams } from "react-router-dom"
 import ListPosts from "../components/ListPosts"
 
 const SubCategoria = () => {
-    const { subcategoria } = useParams()
+    // Obtener el parámetro 'subcategoria' de la URL usando 'useParams' de React Router.
+    const { subcategoria } = useParams();
+
     return (
+        // Renderizamos el componente 'ListPosts' para mostrar los posts de la subcategoría actual.
         <ListPosts url={`/posts?subcategoria=${subcategoria}`} />
-    )
-}
+    );
+};
 
 export default SubCategoria
